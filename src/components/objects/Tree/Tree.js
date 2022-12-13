bfff63a06c3fe7e96f36337996c590ebad823836
Parker Connelly <parkerc@princeton.edu>
2022-12-13 10:04:22 -0500

    lod

--- src/components/objects/Tree/Tree.js
+++ src/components/objects/Tree/Tree.js
@@ -1,31 +1,47 @@
-import {Group, Color, MeshToonMaterial, Texture} from 'three'
+import {Group, Color, MeshToonMaterial, TextureLoader, Vector3} from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import MODEL from './tree.gltf';
-const pineURL = require("pine.jpg").defaut;
+const pineURL = require("./pine.png").default;
+const texture = new TextureLoader().load(pineURL);
+const texMaterial = new MeshToonMaterial({map: texture});
 
 class Tree extends Group {
-    constructor(color){
+    constructor(parent){
         // Call parent Group() constructor
-        super()
-        
-        // Load texture
-        const texture = new
+        super();
+    
+
+
 
         // Load object
         const loader = new GLTFLoader();
-        let object;
-        this.name = 'tree';
-        let meshes = []
         loader.load(MODEL, (gltf) => {
-            let object = gltf.scene;
-            gltf.scene.traverse(e => e.isMesh && meshes.push(e) )
-            meshes[0].material = new MeshToonMaterial({color: new Color(0x6E4400)})
-            meshes[1].material = new MeshToonMaterial({color: new Color(0x082909)})
-            this.add(gltf.scene)
+            gltf.scene.traverse(function(child) {
+                if(child.isMesh) {
+                    if(child.name == "Cylinder001_1") {
+                        child.material = texMaterial;
+                    } else {
+                        child.material = new MeshToonMaterial({color:0x4f2f2f});
+                    }
+                }
+            })
+            this.add(gltf.scene);
         });
         
         
         
+    }
+
+    update(position) {
+        let dist = new Vector3().subVectors(position, this.position);
+        const loader = new GLTFLoader();
+        loader.load(MODEL, (gltf) => {
+            if (dist.length() > 20) {
+                this.material = new MeshToonMaterial({color:0x4f2f2f});
+            } else {
+                this.material = texMaterial;
+            }
+        });
     }
 
 }
